ca3d19fc341d43a7a6d897c2bec96c1732ee790a
S. John <stefan.john@uni-marburg.de>
2023-02-13 14:11:12 +0100

    updated and fixed test cases to distinguish between attributes inside CP node but outside of CP from attributes completely outside of CP

--- src/test/java/recombination/crossoverpoint/CrossoverPointRecombinationTest.java
+++ src/test/java/recombination/crossoverpoint/CrossoverPointRecombinationTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.henshin.interpreter.Engine;
 import org.eclipse.emf.henshin.interpreter.impl.EngineImpl;
 import org.junit.jupiter.api.Test;
@@ -37,7 +38,7 @@ class CrossoverPointRecombinationTest extends GraphTest {
 	private static final IInferenceStrategy MOCKED_INFERENCE = mock(IInferenceStrategy.class);
 	
 	@Test
-	void test_SplitsWithDifferingAttributeInCrossoverPointNoHandler_Exception()
+	void test_SplitsWithDifferingAttributeBeingPartOfCrossoverPoint_Exception()
 			throws GraphManipulationException {
 		// Both parts: Root, A
 		MinimalTestModelBuilder firstCodomainBuilder = TestModelFactory.createMinimalModel();
@@ -63,69 +64,23 @@ class CrossoverPointRecombinationTest extends GraphTest {
 		DefaultAttributeProcessor attributeProcessor = new DefaultAttributeProcessor();
 		CrossoverPointRecombination recombination = new CrossoverPointRecombination(MOCKED_INFERENCE,
 				attributeProcessor, ENGINE);
-		RecombinationException exception = assertThrows(RecombinationException.class,
+		assertThrows(InvalidRecombinationRuleException.class,
 				() -> recombination.recombine(firstSplitBuilder.getSplit(), secondSplitBuilder.getSplit()));
-		assertTrue(exception.getMessage().toLowerCase().contains("attribute"));
 	}
 	
 	@Test
-	void test_SplitsWithDifferingAttributeInCrossoverPointWithHandler_OffspringContainNodeWithProcessedAttribute()
-			throws GraphManipulationException {
-		// Both parts: Root, A
-		MinimalTestModelBuilder firstCodomainBuilder = TestModelFactory.createMinimalModel();
-		((A) (firstCodomainBuilder.aNode.getReferencedObject())).setValue(1);
-		TestSplitBuilder firstSplitBuilder = new TestSplitBuilder(firstCodomainBuilder.graph);
-		firstSplitBuilder.extendBothDomainsBy(firstCodomainBuilder.rootToA);
-		CoSpan firstSplit = firstSplitBuilder.getSplit();
-
-		// Both parts: Root, A
-		MinimalTestModelBuilder secondCodomainBuilder = TestModelFactory.createMinimalModel();
-		((A) (secondCodomainBuilder.aNode.getReferencedObject())).setValue(2);
-		TestSplitBuilder secondSplitBuilder = new TestSplitBuilder(secondCodomainBuilder.graph);
-		secondSplitBuilder.extendBothDomainsBy(secondCodomainBuilder.rootToA);
-		CoSpan secondSplit = secondSplitBuilder.getSplit();
-
-		// Both nodes with edge in cp
-		TestCrossoverPointBuilder crossoverPointBuilder = new TestCrossoverPointBuilder(firstSplit, secondSplit);
-		crossoverPointBuilder.identify(firstCodomainBuilder.rootNode, secondCodomainBuilder.rootNode);
-		crossoverPointBuilder.identify(firstCodomainBuilder.aNode, secondCodomainBuilder.aNode);
-		crossoverPointBuilder.identify(firstCodomainBuilder.rootToA, secondCodomainBuilder.rootToA);
-		
-		saveGraph(firstSplit.getFirstDomain(), "firstSplitFirstPart", false);
-		saveGraph(firstSplit.getSecondDomain(), "firstSplitSecondPart", false);
-		saveGraph(secondSplit.getFirstDomain(), "secondSplitFirstPart", false);
-		saveGraph(secondSplit.getSecondDomain(), "secondSplitSecondPart", false);
-
-		when(MOCKED_INFERENCE.inferCrossoverPoint(any(), any())).thenReturn(crossoverPointBuilder.getCrossoverPoint());		
-
-		CrossoverPointRecombination recombination = new CrossoverPointRecombination(MOCKED_INFERENCE,
-				new SumUpProcessor(), ENGINE);
-		List<EObject> offspring = recombination.recombine(firstSplit, secondSplit);
-
-		// Root, A, A2
-		MinimalTestModelBuilder allExpectedOff = TestModelFactory.createMinimalModel();
-		((A) (allExpectedOff.aNode.getReferencedObject())).setValue(3);
-
-		assertTrue(areIsomorphic(offspring.get(0), allExpectedOff.emfModelRoot));
-		assertTrue(areIsomorphic(offspring.get(1), allExpectedOff.emfModelRoot));
-		saveGraph(ModelGraphFactory.getInstance().createModelGraph(offspring.get(0)), "firstOffspring", false);
-		saveGraph(ModelGraphFactory.getInstance().createModelGraph(offspring.get(1)), "secondOffspring");
-
-	}
-
-	@Test
-	void test_SplitsWithDifferingAttributesOutsideCrossoverPointNoHandler_OffspringShouldContainBothNodesAndTheirAttributes()
+	void test_SplitsWithDifferingAttributesOutsideCrossoverPoint_OffspringShouldContainBothNodesAndTheirAttributes()
 			throws GraphManipulationException {
 		// Both split parts: Root, A
 		MinimalTestModelBuilder firstCodomainBuilder = TestModelFactory.createMinimalModel();
-		((A) (firstCodomainBuilder.aNode.getReferencedObject())).setValue(2);
+		((A) (firstCodomainBuilder.aNode.getReferencedObject())).setValue(0);
 		TestSplitBuilder firstSplitBuilder = new TestSplitBuilder(firstCodomainBuilder.graph);
 		firstSplitBuilder.extendBothDomainsBy(firstCodomainBuilder.rootToA);
 		CoSpan firstSplit = firstSplitBuilder.getSplit();
 
 		// Both split parts: Root, A
 		MinimalTestModelBuilder secondCodomainBuilder = TestModelFactory.createMinimalModel();
-		((A) (secondCodomainBuilder.aNode.getReferencedObject())).setValue(4);
+		((A) (secondCodomainBuilder.aNode.getReferencedObject())).setValue(1);
 		TestSplitBuilder secondSplitBuilder = new TestSplitBuilder(secondCodomainBuilder.graph);
 		secondSplitBuilder.extendBothDomainsBy(secondCodomainBuilder.rootToA);
 		CoSpan secondSplit = secondSplitBuilder.getSplit();
@@ -149,15 +104,98 @@ class CrossoverPointRecombinationTest extends GraphTest {
 		MinimalTestModelBuilder allExpectedOff = TestModelFactory.createMinimalModel();
 		ModelNode a2Node = allExpectedOff.createA();
 		ModelEdge rootToA2 = allExpectedOff.createRootToA(allExpectedOff.rootNode, a2Node);
-		((A) (allExpectedOff.aNode.getReferencedObject())).setValue(2);
-		((A) a2Node.getReferencedObject()).setValue(4);
+		((A) (allExpectedOff.aNode.getReferencedObject())).setValue(0);
+		((A) a2Node.getReferencedObject()).setValue(1);
 
 		assertTrue(areIsomorphic(offspring.get(0), allExpectedOff.emfModelRoot));
 		assertTrue(areIsomorphic(offspring.get(1), allExpectedOff.emfModelRoot));
 		saveGraph(ModelGraphFactory.getInstance().createModelGraph(offspring.get(0)), "firstOffspring", false);
 		saveGraph(ModelGraphFactory.getInstance().createModelGraph(offspring.get(1)), "secondOffspring");
 	}
+	
+	@Test
+	void test_SplitsWithDifferingAttributeOutsideCrossoverPointButInCrossoverPointNodeUnhandled_Exception()
+			throws GraphManipulationException {
+		// Both parts: Root, A
+		MinimalTestModelBuilder firstCodomainBuilder = TestModelFactory.createMinimalModel();
+		((A) (firstCodomainBuilder.aNode.getReferencedObject())).setValue(0);
+		TestSplitBuilder firstSplitBuilder = new TestSplitBuilder(firstCodomainBuilder.graph);
+		firstSplitBuilder.extendBothDomainsBy(firstCodomainBuilder.rootToA);
+		CoSpan firstSplit = firstSplitBuilder.getSplit();
 
+		// Both parts: Root, A
+		MinimalTestModelBuilder secondCodomainBuilder = TestModelFactory.createMinimalModel();
+		((A) (secondCodomainBuilder.aNode.getReferencedObject())).setValue(1);
+		TestSplitBuilder secondSplitBuilder = new TestSplitBuilder(secondCodomainBuilder.graph);
+		secondSplitBuilder.extendBothDomainsBy(secondCodomainBuilder.rootToA);
+		CoSpan secondSplit = secondSplitBuilder.getSplit();
+
+		// Both nodes with edge in cp
+		TestCrossoverPointBuilder crossoverPointBuilder = new TestCrossoverPointBuilder(firstSplit, secondSplit);
+		crossoverPointBuilder.identify(firstCodomainBuilder.rootNode, secondCodomainBuilder.rootNode);
+		crossoverPointBuilder.identify(firstCodomainBuilder.aNode, secondCodomainBuilder.aNode);
+		crossoverPointBuilder.identify(firstCodomainBuilder.rootToA, secondCodomainBuilder.rootToA);
+		EStructuralFeature valueFeature = firstCodomainBuilder.aNode.getReferencedObject().eClass().getEStructuralFeature("value");
+		crossoverPointBuilder.unidentifyAttribute(firstCodomainBuilder.aNode, (EAttribute)valueFeature);
+				
+		saveGraph(firstSplit.getFirstDomain(), "firstSplitFirstPart", false);
+		saveGraph(firstSplit.getSecondDomain(), "firstSplitSecondPart", false);
+		saveGraph(secondSplit.getFirstDomain(), "secondSplitFirstPart", false);
+		saveGraph(secondSplit.getSecondDomain(), "secondSplitSecondPart", false);
+
+		when(MOCKED_INFERENCE.inferCrossoverPoint(any(), any())).thenReturn(crossoverPointBuilder.getCrossoverPoint());		
+
+		CrossoverPointRecombination recombination = new CrossoverPointRecombination(MOCKED_INFERENCE,
+				new DefaultAttributeProcessor(), ENGINE);
+		assertThrows(RecombinationException.class, () -> recombination.recombine(firstSplit, secondSplit));
+	}
+	
+	@Test
+	void test_SplitsWithDifferingAttributeOutsideCrossoverPointButInCrossoverPointNodeWithHandler_OffspringContainsNodeWithProcessedAttribute()
+			throws GraphManipulationException {
+		// Both parts: Root, A
+		MinimalTestModelBuilder firstCodomainBuilder = TestModelFactory.createMinimalModel();
+		((A) (firstCodomainBuilder.aNode.getReferencedObject())).setValue(1);
+		TestSplitBuilder firstSplitBuilder = new TestSplitBuilder(firstCodomainBuilder.graph);
+		firstSplitBuilder.extendBothDomainsBy(firstCodomainBuilder.rootToA);
+		CoSpan firstSplit = firstSplitBuilder.getSplit();
+
+		// Both parts: Root, A
+		MinimalTestModelBuilder secondCodomainBuilder = TestModelFactory.createMinimalModel();
+		((A) (secondCodomainBuilder.aNode.getReferencedObject())).setValue(2);
+		TestSplitBuilder secondSplitBuilder = new TestSplitBuilder(secondCodomainBuilder.graph);
+		secondSplitBuilder.extendBothDomainsBy(secondCodomainBuilder.rootToA);
+		CoSpan secondSplit = secondSplitBuilder.getSplit();
+
+		// Both nodes with edge in cp
+		TestCrossoverPointBuilder crossoverPointBuilder = new TestCrossoverPointBuilder(firstSplit, secondSplit);
+		crossoverPointBuilder.identify(firstCodomainBuilder.rootNode, secondCodomainBuilder.rootNode);
+		crossoverPointBuilder.identify(firstCodomainBuilder.aNode, secondCodomainBuilder.aNode);
+		crossoverPointBuilder.identify(firstCodomainBuilder.rootToA, secondCodomainBuilder.rootToA);
+		EStructuralFeature valueFeature = firstCodomainBuilder.aNode.getReferencedObject().eClass().getEStructuralFeature("value");
+		crossoverPointBuilder.unidentifyAttribute(firstCodomainBuilder.aNode, (EAttribute)valueFeature);
+				
+		saveGraph(firstSplit.getFirstDomain(), "firstSplitFirstPart", false);
+		saveGraph(firstSplit.getSecondDomain(), "firstSplitSecondPart", false);
+		saveGraph(secondSplit.getFirstDomain(), "secondSplitFirstPart", false);
+		saveGraph(secondSplit.getSecondDomain(), "secondSplitSecondPart", false);
+
+		when(MOCKED_INFERENCE.inferCrossoverPoint(any(), any())).thenReturn(crossoverPointBuilder.getCrossoverPoint());		
+
+		CrossoverPointRecombination recombination = new CrossoverPointRecombination(MOCKED_INFERENCE,
+				new SumUpProcessor(), ENGINE);
+		List<EObject> offspring = recombination.recombine(firstSplit, secondSplit);
+
+		// Root, A, A2
+		MinimalTestModelBuilder allExpectedOff = TestModelFactory.createMinimalModel();
+		((A) (allExpectedOff.aNode.getReferencedObject())).setValue(3);
+
+		assertTrue(areIsomorphic(offspring.get(0), allExpectedOff.emfModelRoot));
+		assertTrue(areIsomorphic(offspring.get(1), allExpectedOff.emfModelRoot));
+		saveGraph(ModelGraphFactory.getInstance().createModelGraph(offspring.get(0)), "firstOffspring", false);
+		saveGraph(ModelGraphFactory.getInstance().createModelGraph(offspring.get(1)), "secondOffspring");
+	}
+	
 	private class SumUpProcessor implements IAttributeProcessor {
 		@Override
 		public Object processAttribute(EObject firstParent, EObject secondParent, EAttribute attribute) {
